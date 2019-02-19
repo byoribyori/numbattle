a57bb470a5ca4741b34c858e25f309824a34aaab
byoribyori <hatenaqqq@gmail.com>
2019-02-19 05:16:21 +0000

    待っている人

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index',{name:''});
+router.get('/', function (req, res, next) {
+  res.render('index', { name: '' });
   res.end();
 });
 
-router.post('/',(req,res,next)=>{
-  res.render('index',{name:req.body.name});
+router.post('/', (req, res, next) => {
+  var name = req.body.name;
+  if (!name || name === '匿名') {
+    res.render('index', { name: '' });
+  } else {
+    res.render('index', { name: name });
+  }
   res.end();
 });
 
