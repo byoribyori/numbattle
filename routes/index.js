fdf0b8621684849082283b657f762425aec04331
byoribyori <hatenaqqq@gmail.com>
2019-02-22 08:04:46 +0000

    plan

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var planjs = require('./plan');
 
 router.get('/', function (req, res, next) {
-  res.render('index', { name: '' });
+  res.render('index', { name: '' ,plan:!!planjs.plan.length});
   res.end();
 });
 
 router.post('/', (req, res, next) => {
   var name = req.body.name;
   if (!name || name === '匿名') {
-    res.render('index', { name: '' });
+    res.render('index', { name: '' ,plan:!!planjs.plan.length});
   } else {
-    res.render('index', { name: name });
+    res.render('index', { name: name ,plan:!!planjs.plan.length});
   }
   res.end();
 });
