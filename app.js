9c4c7a203e6039afc4bf6afa0227c5d5d8ca31ee
byoribyori <hatenaqqq@gmail.com>
2019-03-01 06:04:25 +0000

    修正

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var app = express();
 
 app.use('/favicon.ico', express.static('./favicon.ico'));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
